5ce1ba1cd3da8ae383eff56d0eec13c17de604e3
Manthan Gupta <manthangupta109@gmail.com>
2023-12-20 19:32:40 +0530

    Feat: Pipe input from other unix commands

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 	"unicode/utf8"
@@ -13,22 +14,36 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:  "go-wc [filename]",
-	Args: cobra.ExactArgs(1),
+	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		var numberOfBytes, numberOfLines, numberOfWords, numberOfChars int
-		filename := args[0]
-		file, err := os.Open(filename)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "could not open the file: %v", err)
-			os.Exit(-1)
-		}
-		defer file.Close()
+		var filename string
+		var input io.Reader
 
-		scanner := bufio.NewScanner(file)
+		if len(args) == 0 {
+			input = os.Stdin
+		} else {
+			filename = args[0]
+			file, err := os.Open(filename)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "could not open the file: %v", err)
+				os.Exit(-1)
+			}
+			defer file.Close()
+			input = file
+		}
+
+		scanner := bufio.NewScanner(input)
 		getBytesToggle, _ := cmd.Flags().GetBool("getBytesToggle")
 		getLinesToggle, _ := cmd.Flags().GetBool("getLinesToggle")
 		getWordsToggle, _ := cmd.Flags().GetBool("getWordsToggle")
 		getCharsToggle, _ := cmd.Flags().GetBool("getCharsToggle")
+
+		if !getBytesToggle && !getLinesToggle && !getWordsToggle {
+			getBytesToggle = true
+			getLinesToggle = true
+			getWordsToggle = true
+		}
 
 		for scanner.Scan() {
 			if getLinesToggle {
@@ -45,17 +60,19 @@ var rootCmd = &cobra.Command{
 			}
 			if getCharsToggle {
 				line := scanner.Text()
-				numberOfChars += utf8.RuneCountInString(line) + 2 // +2 is to account for newline bytes
+				numberOfChars += utf8.RuneCountInString(line) + 2 // +2 is to account for newline chars
 			}
 		}
-		if getBytesToggle {
-			fmt.Fprintf(os.Stdout, "%6d %s\n", numberOfBytes, filename)
+		if getBytesToggle && getLinesToggle && getWordsToggle {
+			fmt.Fprintf(os.Stdout, "%9d %9d %9d %s\n", numberOfLines, numberOfWords, numberOfBytes, filename)
+		} else if getBytesToggle {
+			fmt.Fprintf(os.Stdout, "%9d %s\n", numberOfBytes, filename)
 		} else if getLinesToggle {
-			fmt.Fprintf(os.Stdout, "%6d %s\n", numberOfLines, filename)
+			fmt.Fprintf(os.Stdout, "%9d %s\n", numberOfLines, filename)
 		} else if getWordsToggle {
-			fmt.Fprintf(os.Stdout, "%6d %s\n", numberOfWords, filename)
+			fmt.Fprintf(os.Stdout, "%9d %s\n", numberOfWords, filename)
 		} else if getCharsToggle {
-			fmt.Fprintf(os.Stdout, "%6d %s\n", numberOfChars, filename)
+			fmt.Fprintf(os.Stdout, "%9d %s\n", numberOfChars, filename)
 		}
 	},
 }
