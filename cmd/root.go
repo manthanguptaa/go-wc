9ba634a54bf98c5f0871a8147746a67081688ff4
Manthan Gupta <manthangupta109@gmail.com>
2023-12-20 20:05:13 +0530

    Refactor: Adding comments and removing repeated code

--- cmd/root.go
+++ cmd/root.go
@@ -11,15 +11,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
+func FormatOutput(getBytesToggle, getLinesToggle, getWordsToggle, getCharsToggle int) {
+
+}
+
 var rootCmd = &cobra.Command{
-	Use:  "go-wc [filename]",
+	Use:  "go-wc [flags] [filename]",
 	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		var numberOfBytes, numberOfLines, numberOfWords, numberOfChars int
 		var filename string
 		var input io.Reader
 
+		// checking if there is an input file or pipe input
 		if len(args) == 0 {
 			input = os.Stdin
 		} else {
@@ -34,32 +38,32 @@ var rootCmd = &cobra.Command{
 		}
 
 		scanner := bufio.NewScanner(input)
-		getBytesToggle, _ := cmd.Flags().GetBool("getBytesToggle")
-		getLinesToggle, _ := cmd.Flags().GetBool("getLinesToggle")
-		getWordsToggle, _ := cmd.Flags().GetBool("getWordsToggle")
-		getCharsToggle, _ := cmd.Flags().GetBool("getCharsToggle")
+		getBytesToggle, _ := cmd.Flags().GetBool("c")
+		getLinesToggle, _ := cmd.Flags().GetBool("l")
+		getWordsToggle, _ := cmd.Flags().GetBool("w")
+		getCharsToggle, _ := cmd.Flags().GetBool("m")
 
-		if !getBytesToggle && !getLinesToggle && !getWordsToggle {
+		// default option when no flag is given in the command
+		if !(getBytesToggle && getLinesToggle && getWordsToggle && getCharsToggle) {
 			getBytesToggle = true
 			getLinesToggle = true
 			getWordsToggle = true
 		}
 
 		for scanner.Scan() {
+			line := scanner.Text()
+			arrayOfBytes := scanner.Bytes()
 			if getLinesToggle {
 				numberOfLines++
 			}
 			if getBytesToggle {
-				arrayOfBytes := scanner.Bytes()
 				numberOfBytes += len(arrayOfBytes) + 2 // +2 is to account for newline bytes
 			}
 			if getWordsToggle {
-				line := scanner.Text()
 				words := strings.Fields(line)
 				numberOfWords += len(words)
 			}
 			if getCharsToggle {
-				line := scanner.Text()
 				numberOfChars += utf8.RuneCountInString(line) + 2 // +2 is to account for newline chars
 			}
 		}
@@ -85,10 +89,10 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolP("getBytesToggle", "c", false, "Outputs the numbers of bytes in the file")
-	rootCmd.Flags().BoolP("getLinesToggle", "l", false, "Outputs the numbers of lines in the file")
-	rootCmd.Flags().BoolP("getWordsToggle", "w", false, "Outputs the numbers of words in the file")
-	rootCmd.Flags().BoolP("getCharsToggle", "m", false, "Outputs the numbers of characters in the file")
+	rootCmd.Flags().BoolP("c", "c", false, "Outputs the numbers of bytes in the file")
+	rootCmd.Flags().BoolP("l", "l", false, "Outputs the numbers of lines in the file")
+	rootCmd.Flags().BoolP("w", "w", false, "Outputs the numbers of words in the file")
+	rootCmd.Flags().BoolP("m", "m", false, "Outputs the numbers of characters in the file")
 }
 
 var RootCmd = rootCmd
