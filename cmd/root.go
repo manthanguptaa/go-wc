4c2a0d8279dd233c091ff3a6fa0e274d89f97423
Manthan Gupta <manthangupta109@gmail.com>
2023-12-20 18:46:37 +0530

    Feat: counting number of words in a file

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -13,7 +14,7 @@ var rootCmd = &cobra.Command{
 	Use:  "go-wc [filename]",
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		var numberOfBytes, numberOfLines int
+		var numberOfBytes, numberOfLines, numberOfWords int
 		filename := args[0]
 		file, err := os.Open(filename)
 		if err != nil {
@@ -25,6 +26,7 @@ var rootCmd = &cobra.Command{
 		scanner := bufio.NewScanner(file)
 		getBytesToggle, _ := cmd.Flags().GetBool("getBytesToggle")
 		getLinesToggle, _ := cmd.Flags().GetBool("getLinesToggle")
+		getWordsToggle, _ := cmd.Flags().GetBool("getWordsToggle")
 
 		for scanner.Scan() {
 			if getLinesToggle {
@@ -34,11 +36,18 @@ var rootCmd = &cobra.Command{
 				arrayOfBytes := scanner.Bytes()
 				numberOfBytes += len(arrayOfBytes) + 2 // +2 is to account for newline bytes
 			}
+			if getWordsToggle {
+				line := scanner.Text()
+				words := strings.Fields(line)
+				numberOfWords += len(words)
+			}
 		}
 		if getBytesToggle {
 			fmt.Fprintf(os.Stdout, "%6d %s\n", numberOfBytes, filename)
 		} else if getLinesToggle {
 			fmt.Fprintf(os.Stdout, "%6d %s\n", numberOfLines, filename)
+		} else if getWordsToggle {
+			fmt.Fprintf(os.Stdout, "%6d %s\n", numberOfWords, filename)
 		}
 	},
 }
@@ -53,6 +62,7 @@ func Execute() {
 func init() {
 	rootCmd.Flags().BoolP("getBytesToggle", "c", false, "Outputs the numbers of bytes in the file")
 	rootCmd.Flags().BoolP("getLinesToggle", "l", false, "Outputs the numbers of lines in the file")
+	rootCmd.Flags().BoolP("getWordsToggle", "w", false, "Outputs the numbers of words in the file")
 }
 
 var RootCmd = rootCmd
