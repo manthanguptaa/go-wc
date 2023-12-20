9c139d9aefcdf73639bb4e02e5ac9e78df51b1aa
Manthan Gupta <manthangupta109@gmail.com>
2023-12-20 18:41:52 +0530

    Feat: count number of lines in a file

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ var rootCmd = &cobra.Command{
 	Use:  "go-wc [filename]",
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		var numberOfBytes int
+		var numberOfBytes, numberOfLines int
 		filename := args[0]
 		file, err := os.Open(filename)
 		if err != nil {
@@ -24,13 +24,22 @@ var rootCmd = &cobra.Command{
 
 		scanner := bufio.NewScanner(file)
 		getBytesToggle, _ := cmd.Flags().GetBool("getBytesToggle")
+		getLinesToggle, _ := cmd.Flags().GetBool("getLinesToggle")
+
 		for scanner.Scan() {
+			if getLinesToggle {
+				numberOfLines++
+			}
 			if getBytesToggle {
 				arrayOfBytes := scanner.Bytes()
 				numberOfBytes += len(arrayOfBytes) + 2 // +2 is to account for newline bytes
 			}
 		}
-		fmt.Fprintf(os.Stdout, "%6d %s\n", numberOfBytes, filename)
+		if getBytesToggle {
+			fmt.Fprintf(os.Stdout, "%6d %s\n", numberOfBytes, filename)
+		} else if getLinesToggle {
+			fmt.Fprintf(os.Stdout, "%6d %s\n", numberOfLines, filename)
+		}
 	},
 }
 
@@ -43,6 +52,7 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().BoolP("getBytesToggle", "c", false, "Outputs the numbers of bytes in the file")
+	rootCmd.Flags().BoolP("getLinesToggle", "l", false, "Outputs the numbers of lines in the file")
 }
 
 var RootCmd = rootCmd
